c4ed18d342f29d24e8dd15794f7263cab08b0f5c
Mauricio Chirino <mchirino@pch.com>
2019-03-17 12:17:39 -0300

    Initial commit

---
+++ contacts/contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contacts
+//
+//  Created by Mauricio Chirino on 17/03/2019.
+//  Copyright © 2019 Mauricio Chirino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
